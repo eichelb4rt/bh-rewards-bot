9c70b95ec2dfaeaedd0f1e0a8e54495bf0305f64
eichelb4rt <maurice.wenig@gmail.com>
2022-09-20 00:19:39 +0200

    changed debug log file name

--- src/debuglog.ts
+++ src/debuglog.ts
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import { Watcher } from "./watcher.js";
 
+function gen_logpath(): string {
+    const now = new Date();
+    return `./logs/${now.getFullYear()}_${now.getMonth() + 1}_${now.getDate()}-${now.getHours()}_${now.getMinutes()}.log`
+}
+
 function sanitize(date: string): string {
     return date.replace(/[\s:]/g, '_').replace(/,/g, '');
 }
@@ -11,12 +16,13 @@ function current_time(): string {
 }
 
 export default class DebugLog {
+    private static readonly lastrun_path = "./logs/last_run.log"
     private static log_filepath: string
     private static session_started: boolean = false;
 
     private static start_session() {
         if (this.session_started) return;
-        this.log_filepath = `./logs/${current_time()}.log`;
+        this.log_filepath = gen_logpath();
         this.session_started = true;
     }
 
@@ -26,6 +32,7 @@ export default class DebugLog {
         const watcher_str = watcher ? ` for ${watcher.user.name}` : '';
         const err_str = `${current_time()}:\tFollowing error occured${time_str}${watcher_str}:\n${err.message}\n${err.stack}\n`;
         fs.appendFileSync(this.log_filepath, err_str);
+        fs.appendFileSync(this.lastrun_path, err_str);
     }
 
     public static log(msg: string, time: string = null, watcher: Watcher = null) {
@@ -34,5 +41,6 @@ export default class DebugLog {
         const watcher_str = watcher ? ` from ${watcher.user.name}` : '';
         const err_str = `${current_time()}:\tMessage${watcher_str}${time_str}: ${msg}\n`;
         fs.appendFileSync(this.log_filepath, err_str);
+        fs.appendFileSync(this.lastrun_path, err_str);
     }
 }
