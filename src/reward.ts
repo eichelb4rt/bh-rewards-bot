dfaa051e860c970d8d203352b0fceb74cf994823
eichelb4rt <maurice.wenig@gmail.com>
2022-09-19 23:40:24 +0200

    fixed harvesting bug: forgot to read rewards before saving

--- src/reward.ts
+++ src/reward.ts
@@ -12,18 +12,26 @@ export class Rewards {
     static #savedRewards: Reward[];
     // set of saved codes for quick access to which codes can be added and which are already saved
     static #savedCodes: Set<string>;
+    // if the file has been checked at least once
+    static fileChecked = false;
 
     static read() {
         if (!fs.existsSync(this.#jsonPath)) {
             this.#savedRewards = [];
+            this.fileChecked = true;
             return;
         }
 
         this.#savedRewards = JSON.parse(fs.readFileSync(this.#jsonPath, 'utf-8'));
         this.#savedCodes = new Set(this.#savedRewards.map(reward => reward.code));
+        this.fileChecked = true;
     }
 
     static save(rewards: Reward[]) {
+        // read it before saving stuff
+        if (!this.fileChecked) {
+            this.read();
+        }
         // filter rewards that we already saved
         rewards = rewards.filter(reward => !this.#savedCodes.has(reward.code));
         // update
